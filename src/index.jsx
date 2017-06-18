4a56269f3a518a39f22c17896144399c41ef180b
Airwoman <airwomanzhy@hotmail.com>
2017-06-19 07:57:51 +1000

    using sweetalert to make the alert box nicely

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,8 @@ import { render } from 'react-dom';
 import ImageComponent from './components/ImageComponent.jsx';
 
 require('./styles/styles.css');
+require('./lib/sweetalert.min.js');
+require('./styles/sweetalert.css');
 require('./index.html');
 
 render(<ImageComponent />, document.getElementById('container'));
